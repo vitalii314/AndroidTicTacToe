f20cda4cb854b7fd982b3db2468c285b3221a195
Vitalik <s.vitalii314@gmail.com>
2016-08-14 20:29:33 +0300

    against player (2 layouts)

--- app/src/main/java/com/vitalii/s/a4tictactoe/MainActivity.java
+++ app/src/main/java/com/vitalii/s/a4tictactoe/MainActivity.java
@@ -2,23 +2,15 @@ package com.vitalii.s.a4tictactoe;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import playground.Playground;
 import playground.SimplePlayGround;
-import playground.State;
 
-public class MainActivity extends Activity {
-
-
-    Button mButtonStartPlayer;
-    TextView mResultText;
-    SimplePlayGround simplePlayground = new SimplePlayGround();
-
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
